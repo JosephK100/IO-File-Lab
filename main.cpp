b9838b3d10588d4b5d6435ffb3c3a0d2fb38962e
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-02-13 02:30:22 +0000

    More changes

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(){
+	std::ifstream csvFile;
+	std::string fileLine;
+	std::stringstream lineParser;
+	std::str fieldOne;
+	std::str fieldTwo;
+	std::str wordField
+	int firstNum;
+	int secondNum;
+	
