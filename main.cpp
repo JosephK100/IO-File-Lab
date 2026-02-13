7bac66aaa0c5cef3b87771c137ccbe05e2dddfdd
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-02-13 21:58:14 +0000

    Final changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 
+//Main - Includes all my variables 
 int main(){
 	std::ifstream csvFile;
 	std::string fileLine;
@@ -14,26 +15,28 @@ int main(){
 //Test to make sure the file is actually opened.	
 	csvFile.open("data.csv");
 	if (!csvFile.is_open()){
-		std::cout << "Issue opening file.\n";
+		std::cout << "Somethin isn't right\n";
 		return 1;
 	}
-//Orignially wanted to use keepGoing for this part, but this works way better.
+//While loop - Reads the csv file one line at a time
 	while (getline(csvFile, fileLine)){
-		lineParser.clear();
+		lineParser.clear();//Reset stringstream and load the current line
 		lineParser.str(fileLine);
 		std::stringstream change;
-		getline(lineParser, fieldOne, ',');//Get line, first int
-		getline(lineParser, fieldTwo, ',');//Get line, second int
-		getline(lineParser, wordField);//Get line, word
-		
+		getline(lineParser, fieldOne, ',');//Parse the first field
+		getline(lineParser, fieldTwo, ',');//Parse the second field
+		getline(lineParser, wordField);//Parse the third field
+//Clear, retrieve, and convert the first number		
 		change.clear();
 		change.str(fieldOne);
 		change >> firstNum;
-
+//Clear, retrieve, and convert the second number
 		change.clear();
 		change.str(fieldTwo);
 		change >> secondNum;
-
+//Add 1st and 2nd number
+//Print the word x amount of times (stored in "sum")
+//Move to the next line with \n 
 		int sum = firstNum + secondNum;
 		for (int i = 0; i < sum; i++){
 			std::cout << wordField << " ";
